6bd301b519c299ad79633936ba795a60d4047aad
Lloyd Draizen Llaban Matirez <144881266+Draizen75@users.noreply.github.com>
2024-05-25 19:35:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,19 +55,7 @@ def download():
     except Exception as e:
         logger.error(f"Error downloading the stream: {str(e)}")
         return f"Error downloading the stream: {str(e)}"
-#     def __init__(self, app, options=None):
-#         self.application = app
-#         self.options = options or {}
-#         super().__init__()
-# 
-#     def load_config(self):
-#         # Apply configuration to Gunicorn
-#         for key, value in self.options.items():
-#             if key in self.cfg.settings and value is not None:
-#                 self.cfg.set(key.lower(), value)
-# 
-#     def load(self):
-#         return self.application
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
